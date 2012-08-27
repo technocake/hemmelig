a465187fc50508deace9425aa647f1d316cbe819
technocake <technocake@komsys.org>
2012-08-27 12:56:33 +0200

    some json

--- webmap/views.py
+++ webmap/views.py
@@ -1,7 +1,7 @@
 from django.template.response import TemplateResponse
 from django.http import HttpResponse, HttpResponseNotAllowed
 from subprocess import Popen, PIPE
-import shlex, sys, re, socket, time
+import shlex, sys, re, socket, simplejson, urllib2, json
 
 
 def index(req):
@@ -15,22 +15,25 @@ def traceroute(req, destination):
     """
     try:
         destination = str(socket.gethostbyname(destination))
+        origin = str(socket.gethostbyname(req.ip))
     except:
         return HttpResponseNotAllowed('<b>!!</b>')
     #ip = '123.3.164.244'
 
-    return HttpResponse(_traceroute(destination))
+    return HttpResponse(_traceroute(destination, origin))
 
 
-def _traceroute(destination):
+def _traceroute(destination, origin):
 
     cmd = shlex.split(
         ('tracert -h 60 -d %s' % destination) if 'win' in sys.platform else
-        'traceroute %s -n -m 60'
-        % destination
-        )
+        ('traceroute %s -n -m 60' % destination)
+    )
     route = Popen(cmd, stdout=PIPE)
+    start, stop = _hop(_lookup(origin), _hop(_lookup(destination)))
 
+    yield start
+    yield stop
     #
     #   Read asyncronously (line-buffered) from the traceroute
     #   stdout
@@ -49,14 +52,29 @@ def _traceroute(destination):
             #
 
             if re.search(dreamland, hop) != None:
-                yield '["dreamland", "-"]\n'
-                if I_HAVE_DREAMT:
-                    yield '["%s", "?"]' % destination
+                yield _hop('dreamland', '')
+
+                if I_HAVE_DREAMT:   # We have already been to dreamland.
+                    yield _hop('destination', '')
                     break
                 I_HAVE_DREAMT = True
             else:
                 m = re.search(hoppat, hop)
                 if m != None:
-                    yield m.groups()
+                    yield _hop(str(m.groups()), '')
         else:
             break
+
+
+def _lookup(host):
+    """ Looks up a ip/host's country """
+    data = urllib2.urlopen('http://geoip.komsys.org/?ip=%s' % host).read()
+    return simplejson.dumps(data)
+
+
+def _hop(ctr, times):
+    return json.dumps('{"ctr" : "%s", "times": "%s"}' % (ctr, times))
+
+
+if __name__ == '__main__':
+    print _lookup('hw.no')
