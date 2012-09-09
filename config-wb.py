ec6bd236d6e4ed9caaade131c7395b0087cd5119
Robin Garen Aaberg <robin.garen@gmail.com>
2012-09-09 23:17:08 +0200

    lol

--- config-wb.py
+++ config-wb.py
@@ -4,5 +4,5 @@ import sys
 class config:
     REMOTE_HOST_URI = "marte.komsys.org:/srv/webmap/"
     REMOTE_WEB_URI = "http://webmap.technocake.net:8000"
-    LOCAL_DIR = r'~/code/webmap/'
+    LOCAL_DIR = r'/cygdrive/c/Users/technocake/django/webmap/'
     DELAY = 1
